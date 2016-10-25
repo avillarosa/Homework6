7f5ba501a86489a8ca94aeaecaefbb453dcca459
avillarosa <blacknike88@gmail.com>
2016-10-25 05:16:59 -0700

    Rearranged element constructors

--- pokemon.cpp
+++ pokemon.cpp
@@ -21,73 +21,58 @@ int Pokemon::get_hp(){
 	return hp;
 }
 
-// Fire Type class - Derived class of Pokemon
-class Fire: protected Pokemon{
+Fire::Fire(int health , int atk, int def, int sAtk, int sDef, int spd){
 
-	// Constructor that performs stat changes for Fire
-	Fire(int health, int atk, int def, int sAtk, int sDef, int spd){
-		hp = health;
-		attack = atk;
-		defense = def;
-		specialAttack = sAtk;
-		specialDefense = sDef;
-		speed = spd;
+	hp = health;
+	attack = atk;
+	defense = def;
+	specialAttack = sAtk;
+	specialDefense = sDef;
+	speed = spd;
 
-		hp += 1;
-		attack += 2;
-		defense -= 2;
-		specialAttack += 4;
-		specialDefense -= 2;
-		speed += 2;
+	hp += 1;
+	attack += 2;
+	defense -= 2;
+	specialAttack += 4;
+	specialDefense -= 2;
+	speed += 2;
 
-	}
+}
 
-};
+Water::Water(int health , int atk, int def, int sAtk, int sDef, int spd){
 
-// Water Type class - Derived Class of Pokemon
-class Water: protected Pokemon{
+	hp = health;
+	attack = atk;
+	defense = def;
+	specialAttack = sAtk;
+	specialDefense = sDef;
+	speed = spd;
 
-	// Constructor that performs stat changes for Water
-	Water(int health, int atk, int def, int sAtk, int sDef, int spd){
-		hp = health;
-		attack = atk;
-		defense = def;
-		specialAttack = sAtk;
-		specialDefense = sDef;
-		speed = spd;
+	hp += 2;
+	attack -= 2;
+	defense += 2;
+	specialAttack -= 1;
+	specialDefense += 4;
+	speed -= 1;
 
-		hp += 2;
-		attack -= 2;
-		defense += 2;
-		specialAttack -= 1;
-		specialDefense += 4;
-		speed -= 1;
+}
 
-	}
-};
+Grass::Grass(int health, int atk, int def, int sAtk, int sDef, int spd){
 
-// Grass Type class - Derived class of Pokemon
-class Grass: protected Pokemon{
+	hp = health;
+	attack = atk;
+	defense = def;
+	specialAttack = sAtk;
+	specialDefense = sDef;
+	speed = spd;
 
-	// Constructor that performs stat changes for Grass
-	Grass(int health , int atk, int def, int sAtk, int sDef, int spd){
-		hp = health;
-		attack = atk;
-		defense = def;
-		specialAttack = sAtk;
-		specialDefense = sDef;
-		speed = spd;
+	hp -= 3;
+	attack -= 2;
+	defense += 0;
+	specialAttack -= 3;
+	specialDefense += 0;
+	speed -= 2;
 
-		hp -= 3;
-		attack -= 2;
-		defense += 0;
-		specialAttack -= 3;
-		specialDefense += 0;
-		speed -= 2;
+}
 
-	}
-};
-
-// Scoped enumeration for the element type, FIRE, WATER, GRASS
-enum struct Element {FIRE, WATER, GRASS};
 
--- pokemon.h
+++ pokemon.h
@@ -5,6 +5,9 @@ using namespace std;
 #ifndef POKEMON_H_
 #define POKEMON_H_
 
+// Scoped enumeration for the element type, FIRE, WATER, GRASS
+enum struct Element {FIRE, WATER, GRASS};
+
 // Pokemon class - Base class
 class Pokemon{
 
@@ -28,6 +31,28 @@ public:
 	int get_hpMax();
 	int get_hp();
 
+};
+
+// Fire Type class - Derived class of Pokemon
+class Fire: protected Pokemon{
+
+	// Constructor that performs stat changes for Fire
+	Fire(int,int,int,int,int,int);
+
+};
+
+// Water Type class - Derived Class of Pokemon
+class Water: protected Pokemon{
+
+	// Constructor that performs stat changes for Water
+	Water(int,int,int,int,int,int);
+};
+
+// Grass Type class - Derived class of Pokemon
+class Grass: protected Pokemon{
+
+	// Constructor that performs stat changes for Grass
+	Grass(int,int,int,int,int,int);
 };
 
 #endif /* POKEMON_H_ */
